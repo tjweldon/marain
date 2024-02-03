12f89318ab3bef7fcb3df2e667cb121dd513f7c6
tjweldon <tomd90@gmail.com>
2024-02-03 02:40:33 +0000

    Testing resetting long running version

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
+use chrono::{Timelike, Utc};
 use std::time;
 
 fn main() {
     // this is a contrived commit
     loop {
         std::thread::sleep(time::Duration::from_secs(5));
-        println!("THE TIME IS NOW {:?}", time::Instant::now());
+        let now = Utc::now();
+        let (is_pm, hour) = now.hour12();
+        println!(
+            "THE TIME IS NOW {:02}:{:02}:{:02} {}",
+            hour,
+            now.minute(),
+            now.second(),
+            if is_pm { "PM" } else { "AM" }
+        );
     }
 }
