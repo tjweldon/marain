5c96c1ac8e3c1f1fb96cb205523b7b23802f7345
Wombatlord <jordan.stone0@gmail.com>
2024-03-07 02:55:37 +0000

    completed encrypt/decrypt round trip for messages broadcast to clients

--- src/handlers/recv_routing.rs
+++ src/handlers/recv_routing.rs
@@ -24,14 +24,13 @@ fn decrypt(user_key: &[u8; 32], enc: Vec<u8>) -> Option<Vec<u8>> {
     }
 }
 
-
 fn deserialize(msg: Vec<u8>) -> Option<ClientMsg> {
     match bincode::deserialize::<ClientMsg>(&msg[..]) {
         Ok(cm) => Some(cm),
         Err(e) => {
             log::warn!("Unrecognised message from client: {e}");
             None
-        },
+        }
     }
 }
 
@@ -51,7 +50,7 @@ pub async fn recv_routing_handler(
                     let Some(decoded) = decrypt(&user_key, msg_bytes) else {
                         return future::ready(());
                     };
-                    
+
                     // can fail if deserialization fails
                     let Some(usr_msg) = deserialize(decoded) else {
                         return future::ready(());
@@ -98,12 +97,12 @@ pub async fn recv_routing_handler(
                 Ok(Message::Close(..)) => {
                     remove_user(room_map.clone(), user.clone());
                 }
-                
+
                 // unhandled message formats
                 Ok(Message::Text(..)) | Ok(Message::Ping(..)) | Ok(Message::Pong(..)) | Ok(Message::Frame(..)) => {
                     log::warn!("Received unhandled message format: Mesage::Text | Message::Ping | Message::Pong | Message::Frame")
                 }
-                
+
                 // upstream connection closed
                 Err(e) => {
                     remove_user(room_map.clone(), user.clone());
